5aac04e6b9672b99fa624312dddf5628299c832f
Nabilbentamer <nabil.bentamer7@gmail.com>
2021-12-15 01:00:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavigationBar from "./Components/NavigationBar";
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+      <p>Bienvenue au Magasin des Voitures</p>
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+class NavigationBar extends React.Component {
+  render() {
+    return;
+    <div>Navigation Bar</div>;
+  }
+}
+export default NavigationBar;
